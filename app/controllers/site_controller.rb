729ddff76d09e7f1bca5ed50a5097e38fa93f617
Tair Assimov <tair@assimov.net>
2011-11-24 19:15:36 +0200

    Implementation of isbn action:
    * The same action handles both GET and POST
    * If request is post? we validate ISBN with our validator
    * Controller shares data with View via instance variables, @isbn in this case
    * If validation succeeds we redirect to the same action with named route
    * If validation fails, we set failure message in flash and render the same view

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,6 +4,16 @@ class SiteController < ApplicationController
   end
 
   def isbn
+    if request.post?
+      @isbn = params[:isbn]
+      if IsbnValidator.new.validate(@isbn)
+        flash[:notice] = "#{@isbn} is a valid ISBN!"
+        redirect_to isbn_validator_path
+      else
+        flash.now[:error] = "#{@isbn} is not a valid ISBN!"
+        render :isbn
+      end
+    end
   end
 
 end
