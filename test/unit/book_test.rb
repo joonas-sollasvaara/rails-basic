d83bc76a5e7654254d5b824d508806f71a00b5ef
Tair Assimov <tair@assimov.net>
2011-11-29 15:55:14 +0200

    Test valid book creation:
    * Setup block runs before each test
    * Fixtures are access with model(:fixture_name)
    * object.valid? validates the instance
    * object.save saves the instance

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @book     = books(:one)
+    @new_book = Book.new(@book.attributes.merge({:title => 'New'}))
+  end
+
+  test "should save valid book" do
+    assert @new_book.valid?
+    assert @new_book.save
+  end
+  
 end
