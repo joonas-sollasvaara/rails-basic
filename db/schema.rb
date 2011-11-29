bf83e2cb8bd8110c92eac0f5bd97977bf226d05f
Tair Assimov <tair@assimov.net>
2011-11-29 16:35:30 +0200

    Add ISBN field to Book model:
    * Generate a migration with rake command:
    rails generate migration AddIsbnToBooks isbn:string
    * Migrate the database:
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111128101743) do
+ActiveRecord::Schema.define(:version => 20111129143417) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20111128101743) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "isbn"
   end
 
 end
