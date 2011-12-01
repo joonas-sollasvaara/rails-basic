055dc95290fcbe36696d995df0c0c7b5ca0424ac
Tero Parviainen <tero.parviainen@eficode.fi>
2011-12-01 12:11:41 +0200

    Creating Books
    * Add link to new book page from book listing
    * Add functional test for the new action
    * Implement the new action
    * Implement the new view
    * Add functional tests for the create action (both valid and invalid case)
    * Implement the create action
    * Add validation error display to the new view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,5 +7,19 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      flash[:notice] = "Book created"
+      redirect_to books_path
+    else
+      render action: :new
+    end
+  end
   
 end
--- test/functional/books_controller_test.rb
+++ test/functional/books_controller_test.rb
@@ -15,5 +15,29 @@ class BooksControllerTest < ActionController::TestCase
     assert assigns(:book)
     assert_equal book, assigns(:book)
   end
+
+  test "new book" do
+    get :new
+    assert_response :success
+    assert assigns(:book)
+    assert assigns(:book).new_record?
+  end
+  
+  test "create book with valid parameters" do
+    assert_difference("Book.count", +1) do
+      post :create, book: {title: 'Programming Your Home', authors: 'Mike Riley', isbn: '978-1-93435-690-6'}
+      assert_response :redirect
+      assert_redirected_to books_path
+      assert flash[:notice]
+    end
+  end
+  
+  test "create book with invalid parameters" do
+    post :create, book: {title: 'No such book'}
+    assert_response :success
+    assert assigns(:book)
+    assert assigns(:book).new_record?
+    assert_template :new
+  end
   
 end
