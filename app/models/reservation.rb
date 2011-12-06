98dad81361e502549580ee100ddc17244da8c9e5
Tair Assimov <tair@assimov.net>
2011-12-06 12:02:31 +0200

    Test and implement Reservation model rules:
    * book_id must always present
    * state can be either "free" or "reserved"
    * email must present and be of valid format

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,9 @@
 class Reservation < ActiveRecord::Base
+  
+  STATES = %w( free reserved )
+  
+  validates :book_id, :presence  => true
+  validates :email,   :presence  => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :state,   :inclusion => { :in => STATES }
+
 end
--- test/unit/reservation_test.rb
+++ test/unit/reservation_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @new_reservation = Reservation.new(
+      book_id: books(:one).id, 
+      email: 'library@eficode.fi',
+      state: 'free'
+    )
+  end
+  
+  test "should save valid reservation" do
+    assert @new_reservation.valid?
+    assert @new_reservation.save
+  end
+  
+  test "should not save reservation without a book_id" do
+    @new_reservation.book_id = nil
+    assert @new_reservation.invalid?
+    assert @new_reservation.errors[:book_id].any?
+    assert !@new_reservation.save
+  end
+  
+  test "should not save reservation with invalid state" do
+    @new_reservation.state = 'invalid'
+    assert !@new_reservation.save
+    assert @new_reservation.errors[:state].any?
+  end
+
+  test "should not save with empty email" do
+    @new_reservation.email = nil
+    assert !@new_reservation.save
+    assert @new_reservation.errors[:email].any?
+  end
+
+  test "should not save with invalid email" do
+    @new_reservation.email = 'invalid@e'
+    assert !@new_reservation.save
+    assert @new_reservation.errors[:email].any?
+  end
+  
 end
