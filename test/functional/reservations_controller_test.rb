dcd93676bd0b816758beaead855f97ecadfe10ec
Tair Assimov <tair@assimov.net>
2011-12-06 12:13:34 +0200

    Creating Reservations:
    * Add link to reserve a book from book page
    * Add functional test for the new action
    * Implement the new action
    * Implement the new view
    * Add functional tests for the create action (both valid and invalid case)
    * Implement the create action
    * Add validation error display to the new view

--- test/functional/reservations_controller_test.rb
+++ test/functional/reservations_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ReservationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @book = books(:one)
+  end
+  
+  test "new reservation" do
+    get :new, book_id: @book.id
+    assert_response :success
+    assert_equal @book, assigns(:book)
+    assert assigns(:reservation)
+    assert assigns(:reservation).new_record?
+  end
+  
+  test "create reservation with valid parameters" do
+    assert_difference("Reservation.count", +1) do
+      post :create, book_id: @book.id, reservation: {email: 'library@eficode.fi'}
+      assert_response :redirect
+      assert_redirected_to book_path(@book)
+      assert flash[:notice]
+    end
+  end
+  
+  test "create reservation with invalid parameters" do
+    post :create, book_id: @book.id, reservation: {email: 'invalid'}
+    assert_response :success
+    assert assigns(:reservation)
+    assert assigns(:reservation).new_record?
+    assert_template :new
+  end
+
 end
