5e741a553518a10017cd2ad4c789e338e0d8570a
Tero Parviainen <tero.parviainen@eficode.fi>
2011-12-07 20:09:37 +0200

    Adding Twitter Bootstrap styling:
    * Add the bootstrap-sass gem
    * bundle install
    * require bootstrap in application.css
    * Make changes to views to apply bootstrap:
      * Container div
      * Topbar
      * Button styling
      * List styling
      * Form styling
      * Validation error styling
      * Flash message styling
    * Use HTML5 search input with placeholder attribute for search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def bootstrap_alert_class(flash_name)
+    if flash_name == :notice
+      "success"
+    elsif flash_name == :error
+      "error"
+    else
+      "info"
+    end
+  end
+  
 end
